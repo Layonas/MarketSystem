c0ff67a732f848359d7e57233c35ff3966410b0d
Layonas <dovdaszl@gmail.com>
2022-07-28 15:34:43 +0300

    new modules added for further development

---
+++ MarketSystem/MarketSystem/Services/ShopCartService.cs
@@ -0,0 +1,110 @@
+﻿using MarketSystem.Models;
+using System.Text.Json;
+
+namespace MarketSystem.Services
+{
+    public class ShopCartService
+    {
+        private IWebHostEnvironment env { get; }
+
+        public ShopCartService(IWebHostEnvironment webHostEnvironment)
+        {
+            this.env = webHostEnvironment;
+        }
+
+        public string fileName
+        { get { return Path.Combine(env.WebRootPath, "data", "shopcart.json"); } }
+
+        public ShopCart? GetShopCart(string id)
+        {
+            if (!File.Exists(fileName))
+                File.Create(fileName);
+
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string json = reader.ReadToEnd();
+                if (json.Length == 0)
+                {
+                    var cart = new ShopCart(id, Enumerable.Empty<Product>());
+                    registerCart(cart);
+                    return cart;
+                }
+                var shopCarts = JsonSerializer.Deserialize<IEnumerable<ShopCart>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                if (shopCarts is null || !shopCarts.Any(cart => cart?.id?.CompareTo(id) == 0))
+                {
+                    var cart = new ShopCart(id, Enumerable.Empty<Product>());
+                    registerCart(cart);
+                    return cart;
+                }
+
+                return shopCarts?.First(cart => cart?.id?.CompareTo(id) == 0);
+            }
+        }
+
+        public void addProductToCart(Product product, string id)
+        {
+            var shopCart = GetShopCart(id);
+
+            if (shopCart is null)
+                return;
+
+            shopCart.products = shopCart?.products?.Append(product);
+        }
+
+        private void updateShoppingCart(ShopCart cart)
+        {
+            var shoppingCarts = GetShopCarts();
+
+            shoppingCarts = shoppingCarts?.Append(cart);
+
+            using (var writer = File.OpenWrite(fileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<ShopCart>>(
+                    new Utf8JsonWriter(writer, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }), shoppingCarts);
+                writer.Close();
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns>null if there are no shoping carts</returns>
+        private IEnumerable<ShopCart>? GetShopCarts()
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                var json = reader.ReadToEnd();
+                var shopCarts = JsonSerializer.Deserialize<IEnumerable<ShopCart>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return shopCarts;
+            }
+        }
+
+        private void registerCart(ShopCart cart)
+        {
+            var shopCarts = GetShopCarts();
+
+            if (shopCarts is null)
+                shopCarts = Enumerable.Empty<ShopCart>();
+
+            shopCarts = shopCarts.Append(cart);
+
+            using (var writer = File.OpenWrite(fileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<ShopCart>>(
+                    new Utf8JsonWriter(writer, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }
+                    ),
+                    shopCarts);
+                writer.Close();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Market System
-If you type the name of a product that is already in the market it will auto complete the price and choose the product for you
-Updated main page design
-Added modal and an option to choose quantity for specific product
+Added ShopCart model  
+Added ShopCartService
